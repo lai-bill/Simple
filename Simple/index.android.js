5c8bb6225c294459776e97353fdcaa7e079e7ab8
bill.lai <bill.lai@wshuttle.com>
2016-03-17 21:48:14 +0800

    添加导航模块

--- Simple/index.android.js
+++ Simple/index.android.js
@@ -3,49 +3,22 @@
  * https://github.com/facebook/react-native
  */
 'use strict';
-import React, {
-  AppRegistry,
+var React = require("react-native");
+var Index = require("./view/Index");
+var {
   Component,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+  AppRegistry,
+  View,
+  Text
+} = React;
 
 class Simple extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Index />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('Simple', () => Simple);
